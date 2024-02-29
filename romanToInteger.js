119dcf7e3177037c1ff78d26204f60380bacd0fd
minhok1 <minhok3@gmail.com>
2024-02-28 20:22:19 -0500

    romanToInteger - described

--- romanToInteger.js
+++ romanToInteger.js
@@ -1,4 +1,5 @@
 var romanToInt = function (s) {
+  // Idea: If a roman numeral is before a value that is bigger than itself, just subtract it. Otherwise, I can just add it to total.
   const characters = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
   var result = 0;
 
