6b335555fbd9d73a489bac6edfb135268fe1ce32
minhok1 <minhok3@gmail.com>
2024-03-01 21:23:55 -0500

    removeDuplicatesFromArray2

---
+++ removeDuplicatesFromArray2.js
@@ -0,0 +1,18 @@
+var removeDuplicates = function (nums) {
+  var p1 = 0;
+  var p2 = 1;
+
+  while (p2 !== nums.length) {
+    console.log(p1, p2);
+    if (nums[p1] !== nums[p2]) {
+      p1 = p2;
+      p2++;
+    } else {
+      if (p2 - p1 < 2) {
+        p2++;
+      } else {
+        nums.splice(p2, 1);
+      }
+    }
+  }
+};
